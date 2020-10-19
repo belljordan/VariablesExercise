9254e8c40930e561fad94ef745a42ad269f5435b
Jordan Bell <jordanbell@MacBook-Pro-100.local>
2020-10-19 12:50:28 -0500

    Completed Excercise

---
+++ variablesExcercise.js
@@ -0,0 +1,19 @@
+let myAge = ("17");
+console.log(myAge);
+
+let CurrentAge = ("17");
+console.log(CurrentAge);
+
+myAge = `${CurrentAge - 5 }`
+console.log(myAge);
+
+myAge = `${CurrentAge - 1}`
+console.log(myAge);
+
+
+myAge = "None of Your business"
+console.log(myAge);
+let blank = ("")
+
+
+
